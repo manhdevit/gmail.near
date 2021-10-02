56e46407cfa48d82cbd212d29d4bc652cec4e568
satohipro <91682706+manhdevit@users.noreply.github.com>
2021-10-02 13:47:14 +0700

    Update index.ts

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -12,18 +12,7 @@ export function getGreeting(accountId: string): string | null {
   return storage.get<string>(accountId, DEFAULT_MESSAGE)
 }
 
-export function setGreeting(message: string): void {
-  const account_id = context.sender
 
-  // Use logging.log to record logs permanently to the blockchain!
-  logging.log(
-    // String interpolation (`like ${this}`) is a work in progress:
-    // https://github.com/AssemblyScript/assemblyscript/pull/1115
-    'Saving greeting "' + message + '" for account "' + account_id + '"'
-  )
-
-  storage.set(account_id, message)
-}
 
 storage.set<number>("vote1",0);
 storage.set<number>("vote2",0);
